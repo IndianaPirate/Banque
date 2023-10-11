10bcb16e7e3078bb1abdda0ec9db374d701e4e9d
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-10-11 07:20:09 +0300

    feat(): class Compte: Constructor methods verser retirer consulterSolde updateSolde totalVersement totalRetraits

---
+++ src/metier/Compte.java
@@ -0,0 +1,56 @@
+package metier;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public abstract class Compte {
+    private int code;
+    private double solde;
+    private List<Operation> operations = new ArrayList<>();
+
+    public Compte() {
+        super();
+    }
+
+    public Compte(int code, double solde, List<Operation> operations) {
+        this.code = code;
+        this.solde = solde;
+        this.operations = operations;
+    }
+    public void verser(double mnt) {
+        Versement v = new Versement(operations.size()+1, new Date(), mnt);
+        operations.add(v);
+        solde += mnt;
+    }
+    public abstract void retirer(double mnt);
+    public void virement(double mnt, Compte compte2){
+        retirer(mnt);
+        compte2.verser(mnt);
+    }
+    public double consulterSolde() {
+        return solde;
+    }
+    public abstract void updateSolde();
+    public List<Operation> getOperations() {
+        return operations;
+    }
+    public double totalVersement() {
+        double somme = 0;
+        for(Operation o: operations) {
+            if (o instanceof Versement) {
+                somme = somme + o.getMontant();
+            }
+        }
+        return somme;
+    }
+    public double totalRetraits() {
+        double somme = 0;
+        for(Operation o: operations) {
+            if (o instanceof Retrait) {
+                somme = somme + o.getMontant();
+            }
+        }
+        return somme;
+    }
+}
